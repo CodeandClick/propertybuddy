aff40191a87d5c5ea5ffd48f9a7e4ed2256bdd3e
Muhammed-Nishan-K <nishank855@gmail.com>
2024-08-14 15:48:20 +0530

    problem in request

--- Admin_frontend/src/services/api.js
+++ Admin_frontend/src/services/api.js
@@ -4,14 +4,11 @@ const checkadmin = async (email, password) => {
     console.log(email,password);
     
   try {
-    const response = await axios.get('', { email, password }); // Replace with your API endpoint
-    // Handle successful response
-    console.log(response.data);
-    return response.data; // Return data if needed
+    const response = await axios.post('http://localhost:4000/admin/login', { email:email, password:password }); 
+    return response.data;
   } catch (error) {
-    // Handle errors
     console.error(error);
-    return null; // Or throw an error
+    return null;
   }
 };
 
